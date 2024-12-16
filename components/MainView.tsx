b596fb2088efd02f7c925904be0868ece5ac9a79
Your Name <ekka.beaula@gmail.com>
2024-12-16 10:31:10 +0100

    maincomponent-start

---
+++ components/MainView.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function MainView() {
+  return (
+    <div className='flex'>
+        {/* Sidebar */}
+        <div className="w-full"></div>
+      
+    </div>
+  )
+}
