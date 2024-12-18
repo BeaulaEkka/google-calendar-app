851699e630b4764f544ccb0d7b0cd07aa8cc8de7
Your Name <ekka.beaula@gmail.com>
2024-12-18 23:56:57 +0100

    set dayView

--- components/MainView.tsx
+++ components/MainView.tsx
@@ -1,14 +1,22 @@
+"use client";
 import React from "react";
 import Sidebar from "./sidebar/Sidebar";
 import MonthView from "./MonthView";
+import { useViewStore } from "@/lib/store";
+import WeekView from "./ui/WeekView";
+import DayView from "./ui/DayView";
 
 export default function MainView() {
+  const { selectedView } = useViewStore();
+  
   return (
     <div className="flex">
       {/* Sidebar */}
       <Sidebar />
       <div className="w-full flex-1">
-        <MonthView />
+        {selectedView === "month" && <MonthView />}
+        {selectedView === "week" && <WeekView />}
+        {selectedView === "day" && <DayView />}
       </div>
     </div>
   );
--- components/header/HeaderRight.tsx
+++ components/header/HeaderRight.tsx
@@ -17,9 +17,9 @@ export default function HeaderRight() {
             <SelectValue placeholder="Theme" />
           </SelectTrigger>
           <SelectContent>
-            <SelectItem value="light">Day</SelectItem>
-            <SelectItem value="dark">Month</SelectItem>
-            <SelectItem value="system">Year</SelectItem>
+            <SelectItem value="day">Day</SelectItem>
+            <SelectItem value="week">Week</SelectItem>
+            <SelectItem value="month">Month</SelectItem>
           </SelectContent>
         </Select>
       </section>
