d4e0ec4618cf2e9d2da9e55ef6f3dcb006e0ab4d
Your Name <ekka.beaula@gmail.com>
2024-12-24 23:01:34 +0100

    installed scrollArea -schadcn

--- components/ui/WeekView.tsx
+++ components/ui/WeekView.tsx
@@ -1,10 +1,49 @@
+"use client";
 import { getWeekDays } from "@/lib/getTime";
 import { useDateStore } from "@/lib/store";
-import React from "react";
+import { cn } from "@/lib/utils";
+import dayjs from "dayjs";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import React, { useEffect, useState } from "react";
 
 export default function WeekView() {
+  const [currentTime, setCurrentTime] = useState(dayjs());
   const { userSelectedDate } = useDateStore();
-  const Days = getWeekDays(userSelectedDate);
-  console.log("Days", Days);
-  return <></>;
+  // const Days = getWeekDays(userSelectedDate);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(dayjs());
+    }, 60000); //every minute
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <div className="grid grid-cols-[auto_1fr_1fr_1fr_1fr_1fr_1fr_1fr] place-items-center px-4 ">
+        <div className="w-16 border-r border-gray-300">
+          <div className="relative h-16">
+            <div className="absolute top-[40%] text-xs text-gray-600">Mon</div>
+          </div>
+        </div>
+        {/**weekview */}
+        {getWeekDays(userSelectedDate).map(({ currentDate, today }, index) => (
+          <div className="flex flex-col" key={index}>
+            <div className={cn("text-xs", today && "text-blue-600")}>
+              {currentDate.format("ddd")}
+            </div>
+            <div
+              className={cn("text-lg", today && "text-blue-600")}
+              key={index}
+            >
+              {currentDate.format("DD")}
+            </div>
+          </div>
+        ))}
+      </div>
+      {/* Time column $ corresponding Bodes of time per date */}
+      <ScrollArea></ScrollArea>
+      <div></div>
+    </>
+  );
 }
