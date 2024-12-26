5a853c0011fccb6396751123105d38e10a85d48d
Your Name <ekka.beaula@gmail.com>
2024-12-27 00:46:30 +0100

    update DayView

--- components/ui/DayView.tsx
+++ components/ui/DayView.tsx
@@ -1,17 +1,32 @@
 import { getWeekDays } from "@/lib/getTime";
 import { useDateStore } from "@/lib/store";
-import React from "react";
+import { cn } from "@/lib/utils";
+import dayjs from "dayjs";
+import React, { useEffect, useState } from "react";
 
 export default function DayView() {
+  const [currentTime, setCurrentTime] = useState(dayjs());
   const { userSelectedDate } = useDateStore();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(dayjs());
+    }, 60000); //every minute
+    return () => clearInterval(interval);
+  }, []);
+
+  const isToday =
+    userSelectedDate.format("DD-MM-YY") === dayjs().format("DD-MM-YY");
+
   return (
     <>
-      <div className="grid grid-cols-[auto_auto_1fr] px-4 border">
-        <div className="">Time</div>
-        <div>
-          <div>cc</div>
-          <div>vv</div>
+      <div className="grid grid-cols-[auto_auto_1fr] px-4 ">
+        <div className="w-16 border-r border-gray-300">Time</div>
+        <div className="flex w-16 flex-col items-center">
+          <div className={cn("text-xs", isToday && "text-blue-600")}>{userSelectedDate.format('ddd')}</div>
+          <div>{userSelectedDate.format('DD')}</div>
         </div>
+        <div></div>
       </div>
     </>
   );
