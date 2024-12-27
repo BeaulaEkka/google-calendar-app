c61d2b1ca100095ef406555a26476e11d53fd55e
Your Name <ekka.beaula@gmail.com>
2024-12-27 23:58:03 +0100

    header-left-update

--- components/header/HeaderLeft.tsx
+++ components/header/HeaderLeft.tsx
@@ -1,10 +1,17 @@
+'use client'
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import { TbMenu2 } from "react-icons/tb";
+import { useDateStore } from "@/lib/store";
+import dayjs from "dayjs";
 
 export default function HeaderLeft() {
   // const date=New Date();
+  const todaysDate = dayjs();
+  const { userSelectedDate, setDate, setMonth, selectedMonthIndex } =
+    useDateStore();
+    
   return (
     <div className="flex gap-2 items-center ">
       <section>
@@ -14,7 +21,7 @@ export default function HeaderLeft() {
         <img src="images/calendar_1_2x.png" alt="date" className="size-12" />
         <p className="font-semibold">Calendar</p>
       </section>
-      <Button variant="outline">Button</Button>
+      <Button variant="outline">Today</Button>
       <section className="flex gap-5">
         <IoIosArrowBack className="size-5" />
         <IoIosArrowForward className="size-5" />
