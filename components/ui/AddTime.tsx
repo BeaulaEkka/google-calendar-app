74163ac614d1b94ca649843159b7ed4c9ce00977
Your Name <ekka.beaula@gmail.com>
2024-12-30 21:41:33 +0100

    generateTimeIntervals

--- components/ui/AddTime.tsx
+++ components/ui/AddTime.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect, useRef } from "react";
 
 export default function AddTime() {
-  return <div>
-    <h1> Add Time </h1>
-  </div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedTime, setSelectedTime] = useState("00:00");
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("click", handleOutsideClick);
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+
+  const generateTimeIntervals = () => {
+    const intervals = [];
+    for (let hour = 0; hour < 24; hour++) {
+      for (let minute = 0; minute < 60; minute += 15) {
+        intervals.push(
+          `${hour.toString().padStart(2, "0")}:${minute
+            .toString()
+            .padStart(2, "0")}`
+        );
+      }
+    }
+    return intervals;
+  };
+
+  handleTimeSelect=(time:string)
+
+  return (
+    <div>
+      <h1> Add Time </h1>
+    </div>
+  );
 }
